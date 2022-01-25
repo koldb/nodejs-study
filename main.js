1725f711e31ef777b9ad06790855458dcdf769e6
koldb <chosss4@naver.com>
2022-01-25 19:37:37 +0900

    node.js 5일차

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const path = require('path/posix');
  
 function templateHTML(title, list, body){
   return`
@@ -13,6 +14,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
   </html>
@@ -42,10 +44,9 @@ var app = http.createServer(function(request,response){
           var description = 'Hello, Node.js';
           var list = templateLIST(filelist);
           var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
-          console.log("WEB HOME");
           response.writeHead(200);
           response.end(template);
-        })
+        });
       } else {
         fs.readdir('./data', function(error, filelist){
           var list = templateLIST(filelist);
@@ -57,6 +58,25 @@ var app = http.createServer(function(request,response){
           });
         });
       }
+    } else if(pathname === '/create'){
+      fs.readdir('./data', function(error, filelist){
+        var title = 'WEB - Create';
+        var list = templateLIST(filelist);
+        var template = templateHTML(title, list, `
+        <form action="http://localhost:3000/preocess_create" method="post">
+
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+            <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+            <input type="submit">
+        </p>
+        </form>
+        `);
+        response.writeHead(200);
+        response.end(template);
+      });
     } else {
       response.writeHead(404);
       response.end('Not found');
